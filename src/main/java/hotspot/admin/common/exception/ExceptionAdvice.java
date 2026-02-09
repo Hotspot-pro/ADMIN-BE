d8c09c4052c3111e4dab08093b4a5784e03bcaed
andrew <leegd120@gmail.com>
2026-02-10 02:50:00 +0900

    fix : spotless 및 checkstyle 에러 해결

--- src/main/java/hotspot/admin/common/exception/ExceptionAdvice.java
+++ src/main/java/hotspot/admin/common/exception/ExceptionAdvice.java
@@ -1,14 +1,16 @@
 package hotspot.admin.common.exception;
 
-import hotspot.admin.common.exception.code.BaseErrorCode;
-import hotspot.admin.common.exception.code.GlobalErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import hotspot.admin.common.exception.code.BaseErrorCode;
+import hotspot.admin.common.exception.code.GlobalErrorCode;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
